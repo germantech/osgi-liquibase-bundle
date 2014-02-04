6c654ce250dcbb3e879978b19af95fea78dffcd7
balazs-zsoldos <balazs.zsoldos@everit.biz>
2014-02-04 18:05:16 +0100

    License header and eosgi-maven-plugin update

--- tests/src/main/java/org/everit/osgi/liquibase/bundle/tests/ConfigurationInitComponent.java
+++ tests/src/main/java/org/everit/osgi/liquibase/bundle/tests/ConfigurationInitComponent.java
@@ -1,25 +1,20 @@
-package org.everit.osgi.liquibase.bundle.tests;
-
-/*
- * Copyright (c) 2011, Everit Kft.
+/**
+ * This file is part of Everit Liquibase OSGi Bundle Tests.
  *
- * All rights reserved.
+ * Everit Liquibase OSGi Bundle Tests is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
+ * Everit Liquibase OSGi Bundle Tests is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Everit Liquibase OSGi Bundle Tests.  If not, see <http://www.gnu.org/licenses/>.
  */
+package org.everit.osgi.liquibase.bundle.tests;
 
 import java.io.IOException;
 import java.util.Dictionary;
